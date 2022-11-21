3da2356f8c0b7b7261f06b2df7be2a9aeeb348c0
tiara <93997755+4utiara@users.noreply.github.com>
2022-11-21 12:41:05 +0530

    Create quicksort.java

---
+++ quicksort.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+//program wrt to abdul bari sir for quick sort//
+
+//function for partition//
+class partition {
+    int i, j, l = 0, h;
+    int pivot;
+    Scanner sc = new Scanner(System.in);
+   int[] array =  new int[10] ;for(i=0;i<10;i++){
+    array[i]=sc.nextInt();
+
+   }
+
+    // getting to the partition algorithm//
+    int partition(int i, int j) {
+
+    }
+}
